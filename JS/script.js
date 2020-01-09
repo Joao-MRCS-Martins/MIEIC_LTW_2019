52639704369577e6f551dffc8b60ec173442fe64
João Martins <up201707311@fe.up.pt>
2020-01-09 18:23:29 +0000

    added some js exercises in preparation for the exam

---
+++ JS/script.js
@@ -0,0 +1,44 @@
+const form = document.getElementsByTagName('form')[0]//.outerHTML
+// console.log(form)
+// const input = document.querySelector('form label:nth-child(2) input')
+// const inputs = document.querySelectorAll('form input')
+
+// console.log(form)
+// console.log(input)
+// inputs.forEach(input => {
+//   console.log(input.outerHTML)
+// })
+
+form.addEventListener('submit', function (event) {
+  let row = document.createElement('tr')
+  const descr = document.querySelector('form input[name=description]').value
+  const quantity = document.querySelector('form input[name=quantity]').value
+  row.innerHTML = `<tr><td>${descr}</td><td><input value="${quantity}"></td><td><input type="button" value="Remove"></td></tr>`
+  let table = document.querySelector('#products')
+  table.append(row)
+  updateTotal()
+
+  let inputQnt = row.querySelector('td:nth-child(2) input')
+  inputQnt.addEventListener('change', function() {
+    updateTotal()
+  })
+  
+  let remBtn = row.querySelector('input[type=button]')
+  remBtn.addEventListener('click', function () {
+    this.parentNode.parentNode.remove()
+    updateTotal()
+  })
+
+  event.preventDefault()
+})
+
+function updateTotal() {
+  const items = document.querySelectorAll('table tr td:nth-child(2) input')
+  let total = 0
+  items.forEach(input => {
+    total += parseInt(input.value)
+  })
+
+  let totalSpan = document.querySelector('#total')
+  totalSpan.innerHTML = total
+}
